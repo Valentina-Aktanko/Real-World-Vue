2deeaeaa03e77edafb2b87ea437826b92a72d626
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2020-11-07 21:53:46 +0300

    creates a base of components, configures routes, updates links

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,28 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import EventList from "../views/EventList.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "event-list",
+    component: EventList
   },
   {
-    path: "/about-us",
-    name: "About",
+    path: "/event",
+    name: "event-show",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-    alias: "/about"
+      import(/* webpackChunkName: "about" */ "../views/EventShow.vue")
+  },
+  {
+    path: "/event/create",
+    name: "event-create",
+    component: () => import("../views/EventCreate.vue")
   }
 ];
 
