b6707c2c2b8becc77cdff7efa518381e5204ed25
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2020-11-07 22:54:22 +0300

    updates config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,29 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   extends: [
     "plugin:vue/essential",
     "plugin:prettier/recommended",
     "eslint:recommended",
     "@vue/prettier"],
+
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   },
+
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
+    'no-console': 'off',
+    'no-debugger': 'off'
+  },
+
+  'extends': [
+    'plugin:vue/strongly-recommended',
+    'plugin:prettier/recommended',
+    'eslint:recommended',
+    '@vue/prettier'
+  ]
 };
